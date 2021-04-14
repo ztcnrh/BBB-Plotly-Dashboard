036842e41316d42ba7faaa43a876a71f557cec4a
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-04-13 22:30:03 -0400

    Part1 BarChart completed. Add some starter codes for part 2: Display each key-value pair from the metadata... To be continued...

--- app.js
+++ app.js
@@ -1,71 +1,102 @@
-
-selection = d3.select("#selDataset");
-selection.on("change", updateBarChart());
-
-// function optionChanged() {
-
-// }
+// Render the "Top 10 OTUs Sample Values" bar chart
+initBarChart();
 
 
-function updateBarChart() {
+// Function to render the default bar chart before anything is selected in the dropdown menu
+function initBarChart() {
 
+    // Load data
     d3.json("data/samples.json").then((data) => {
 
-        // Populate the dropdown menu with IDs
-        getData(data.samples);
-        console.log(data.samples);
-    
+        // Append test subject IDs into the HTML <select> tag so we have a complete dropdown menu
+        var dropdownMenu = d3.select("#selDataset");
+        // Append <option> tags with test subject IDs
+        data.samples.forEach((subject) => {
+            var option = dropdownMenu.append("option");
+            option.text(subject.id).attr("value", `${subject.id}`);
+        })
+
+        // Select an initial ID for rendering the default bar chart
+        selectedId = getId();
+
         // Filter to include only the data for the selected sample ID
-        var filteredData = data.samples.filter(subject => subject.id === getId());
+        var filteredData = data.samples.filter(subject => subject.id === selectedId);
 
+        
+        // ----------------------------------------------
+        // Panel: Demographic Info
+        panel = d3.select("#sample-metadata");
 
-    // Trace1 --> Top 10 OTUs sample values: Horizontal Bar chart
-    var trace1 = {
-        x: filteredData[0].sample_values.slice(0, 10).map(value => value).reverse(),
-        y: filteredData[0].otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
-        text: filteredData[0].otu_labels.slice(0, 10).map(label => label).reverse(),
-        type: "bar",
-        orientation: "h"
-    };
+        // Filter to include only the metadata for the selected sample ID
+        filteredMetaData = data.metadata.filter(subject => subject.id == selectedId);
 
-    var data = [trace1];
+        // Append <p> tags with key-value paired metadata
+        Object.entries(filteredMetaData[0]).forEach(([key, value]) => {
+            var demInfo = panel.append("p");
+            demInfo.text(`${key}: ${value}`);
+        })
+        // ----------------------------------------------
+        // Plot 1: Trace1 --> Horizontal Bar chart for "Top 10 OTUs Sample Values"
+        var trace1 = {
+            x: filteredData[0].sample_values.slice(0, 10).map(value => value).reverse(),
+            y: filteredData[0].otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+            text: filteredData[0].otu_labels.slice(0, 10).map(label => label).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
 
-    var layout = {
-        xaxis: { title: "Sample Values"},
-        width: 550,
-        height: 500,
-        margin: {
-        l: 150,
-        r: 150,
-        b: 50,
-        t: 50
+        var data = [trace1];
+
+        var layout = {
+            title: "Top 10 OTUs Sample Values",
+            xaxis: { title: "Sample Values"},
+            width: 550,
+            height: 500,
+            margin: {
+            l: 150,
+            r: 150,
+            b: 50,
+            t: 50
+            }
         }
-    }
-    
-    // Render the plot in the corresponding html element
-    Plotly.newPlot("bar", data, layout, {displayModeBar: false});
-    
+
+        // Render the plot in the corresponding html element
+        Plotly.newPlot("bar", data, layout, {displayModeBar: false});
+        // ----------------------------------------------
     })
 }
 
-
-// Function to return the currently selected test subject ID#
+// Function to grab the first ID for rendering the default bar chart when page is loaded
 function getId() {
-    var dropdownMenu = d3.selectAll("#selDataset");
+    var dropdownMenu = d3.select("#selDataset");
     var selectedId = dropdownMenu.property("value");
-    console.log(selectedId);
     return selectedId;
 }
 
-// Function to populate the dropdown menu with all sample IDs
-function getData(arr) {
-    // Target the <select> tag
-    var dropdownMenu = d3.select("#selDataset");
-    // Append <option> tags to contain all sample IDs
-    arr.forEach((subject) => {
-        var option = dropdownMenu.append("option");
-        option.text(subject.id);
+// Function to re-render plot when an option is selected (or changed)
+function updateBarChart(selectedId) {
+
+    // Load data
+    d3.json("data/samples.json").then((data) => {
+
+        // Filter to include only the data for the selected sample ID
+        var filteredData = data.samples.filter(subject => subject.id === selectedId);
+
+        var x = filteredData[0].sample_values.slice(0, 10).map(value => value).reverse();
+        var y = filteredData[0].otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
+        var text = filteredData[0].otu_labels.slice(0, 10).map(label => label).reverse();
+        
+        // Restyle bar chart
+        Plotly.restyle("bar", "x", [x]);
+        Plotly.restyle("bar", "y", [y]);
+        Plotly.restyle("bar", "text", [text]);
     })
 }
-  
+
+// Function to run when HTML <select onchange=""> is called upon
+function optionChanged(idValue) {
+    updateBarChart(idValue);
+    // updateMetaData(id);
+}
+
 
